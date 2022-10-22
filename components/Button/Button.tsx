aa9445ea95c6ee93e49233800fe916d905a3c098
Parsadanashvili <61390801+Parsadanashvili@users.noreply.github.com>
2022-10-22 20:23:24 +0400

    update component(Button)

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,84 +1,51 @@
 import Link, { LinkProps } from "next/link";
-import React, { ForwardedRef, forwardRef, ReactNode } from "react";
+import React, {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  ForwardedRef,
+  forwardRef,
+  ReactNode,
+} from "react";
 import styles from "./Button.module.css";
 
-type ButtonProps = JSX.IntrinsicElements["button"] & {
-  color?: "primary" | "success" | "danger";
-  href?: undefined;
-  children: ReactNode;
+const colorClassnames = {
+  primary: styles.primary,
+  success: styles.success,
+  danger: styles.danger,
 };
 
-type AnchorProps = JSX.IntrinsicElements["a"] & {
-  color?: "primary" | "success" | "danger";
-  href: string;
-  children: ReactNode;
-} & LinkProps;
+// export type ButtonProps = JSX.IntrinsicElements["button"] & {
+//   color?: keyof typeof colorClassnames;
+//   href?: undefined;
+//   children: ReactNode;
+// };
 
-type PolymorphicProps = ButtonProps | AnchorProps;
-type PolymorphicButton = {
-  (props: AnchorProps): JSX.Element;
-  (props: ButtonProps): JSX.Element;
+export type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  color?: keyof typeof colorClassnames;
+  icon?: ReactNode;
 };
 
-const isAnchor = (props: PolymorphicProps): props is AnchorProps => {
-  return props.href != undefined;
-};
-
-const Button = forwardRef<
-  HTMLButtonElement | HTMLAnchorElement,
-  PolymorphicProps
->((props, ref) => {
-  if (isAnchor(props)) {
-    const {
-      href,
-      as,
-      replace,
-      scroll,
-      shallow,
-      passHref,
-      prefetch,
-      locale,
-      color,
-      ...rest
-    } = props;
-    const linkProps = {
-      href,
-      as,
-      replace,
-      scroll,
-      shallow,
-      passHref,
-      prefetch,
-      locale,
-    };
-
-    return (
-      <Link {...linkProps}>
-        <a
-          className={
-            styles.root +
-            (color && styles[color] != undefined ? " " + styles[color] : "")
-          }
-          {...rest}
-          ref={ref as ForwardedRef<HTMLAnchorElement>}
-        />
-      </Link>
-    );
-  }
-
+const Button: React.FC<ButtonProps> = ({
+  children,
+  color = "primary",
+  disabled,
+  icon,
+  className = "",
+  ...props
+}) => {
   return (
     <button
-      {...props}
-      className={
-        styles.root +
-        (props.color && styles[props.color] != undefined
-          ? " " + styles[props.color]
-          : "")
-      }
-      ref={ref as ForwardedRef<HTMLButtonElement>}
+      disabled={disabled}
+      className={`${styles.root} ${colorClassnames[color]} ${className}`}
       type={props.type || "button"}
-    />
+      {...props}
+    >
+      {children}
+    </button>
   );
-});
+};
 
 export default Button;
